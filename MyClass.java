9af0b163bc55173128b6550dd67c7578b8356f34
Netto Cavalcanti <netto.cavalcanti@gmail.com>
2018-05-23 20:52:34 -0300

    Versão do programa controlada por variável estática

--- MyClass.java
+++ MyClass.java
@@ -1,9 +1,11 @@
 public class MyClass {
+    private static final String version = "1.0";
+    
     public static void main(String args[]) {
         System.out.println("Hello, world!");
     }
     
     private String getVersion() {
-        return "1.0";
+        return version;
     }
 }
